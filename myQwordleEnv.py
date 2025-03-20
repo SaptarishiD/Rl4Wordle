5c52bc42c235f2ee6cbbec43494a08b298141c29
SaptarishiD <saptarishi.dhanuka@gmail.com>
2025-03-21 04:27:31 +0530

    trying heuristics

--- myQwordleEnv.py
+++ myQwordleEnv.py
@@ -1,3 +1,4 @@
+# <myQwordleEnv.py>
 import pandas as pd
 import numpy as np
 import gymnasium as gym
@@ -26,12 +27,16 @@ class WordleQEnv():
         self.row_present = [None]*self.word_len # yellows
         self.row_absent = [None]*self.word_len  # blacks
 
+        self.win = ''
+
+
+
         # self.correct
 
     def make_guess(self, word):
         self.attempts += 1
-        if word == self.target_word:
-            print("\n ============= You Won =========== \n")
+        # if word == self.target_word:
+            # print("\n ============= You Won =========== \n")
 
 
 
@@ -60,24 +65,29 @@ class WordleQEnv():
                 self.row_absent[i] = guessed_letter
                 if guessed_letter not in self.letters_absent:
                     self.letters_absent.append(guessed_letter)
-        if self.target_word == word:
-            print("YOU WON!")
-            return
-        
+
         number_of_greens = len([x for x in self.row_correct if x is not None])
         number_of_yellows = len([x for x in self.row_present if x is not None])
         number_of_blacks = len([x for x in self.row_absent if x is not None])
 
+        if self.target_word == word:
+            self.win = True
+            # print(f"YOU WON! In {self.attempts} moves")
+            return number_of_greens, number_of_yellows, number_of_blacks
+        
+       
 
         if self.attempts == self.max_attempts:
-            print("ATTEMPTS FINISHED!")
-            return 
+            self.win = False
+            self.attempts = 7
+            # print("ATTEMPTS FINISHED!")
+            return number_of_greens, number_of_yellows, number_of_blacks
 
         return number_of_greens, number_of_yellows, number_of_blacks
     
 
 class MyAgent():
-    def __init__(self, word_list_path):
+    def __init__(self, word_list_path='target_words.txt'):
         self.agent_guesses = []
         self.agent_attempts = 0
         with open(word_list_path, 'r') as f:
@@ -87,19 +97,76 @@ class MyAgent():
         rand_word = random.choice(self.word_list)
         while (rand_word in self.agent_guesses):
             rand_word = random.choice(self.word_list)
+        return rand_word
 
-
-
-
-
+    def rand_not_absent(self, absent_letters):
+        new_search_space = [word for word in self.word_list if not any(letter in word for letter in absent_letters)]
+        rand_word = random.choice(new_search_space)
+        while (rand_word in self.agent_guesses):
+            rand_word = random.choice(new_search_space)
+        return rand_word
+    
+    def rand_green_not_absent(self, green_positions, absent_letters):
+        filtered = []
+        new_search_space = [word for word in self.word_list if not any(letter in word for letter in absent_letters)]
+        for word in new_search_space:
+            match = True
+            for i, letter in enumerate(green_positions):
+                if letter is not None and word[i] != letter:
+                    match = False
+                    break
+            if match:
+                filtered.append(word)
+        # print(filtered)
+        # print(len(filtered))
+        return random.choice(filtered)
         
+    
 
 
+class WordleMetaEnv():
+    def __init__(self):
+        self.win_reward = 10
+        self.lose_cost = -10
+        self.green_reward = 5
+        self.yellow_reward = 3
+        self.black_cost = -1
 
+        self.total_reward = 0
+        self.action_space = [0,1,2]
+    
+    def reset(self):
+        self.agent = MyAgent()
+        self.env = WordleQEnv()
+        self.guesses_made = 0
 
+        return (0,0,0)  # corresponding to greens and yellows
+    
+    def step(self, action):
+        self.guesses_made += 1
 
+        if action == 0:
+            guess = self.agent.randomly()
+        elif action == 1:
+            guess = self.agent.rand_not_absent(self.env.letters_absent)
+        elif action == 2:
+            guess = self.agent.rand_green_not_absent(self.env.pos_guessed_correctly, self.env.letters_absent)
+        
+        greens, yellows, blacks = self.env.make_guess(guess)
+        
+        reward = self.green_reward * greens + self.yellow_reward * yellows - self.black_cost * blacks
 
+        state = (greens, yellows, blacks)
+        
+        
+        if self.env.win:
+            reward += self.win_reward
+            return state, reward, True
+        elif not self.env.win:
+            reward -= self.lose_cost
+            return state, reward, True
 
+        return state, reward, False
 
 
 
@@ -108,4 +175,9 @@ print(f"Target Word: {env.target_word}")
 env.make_guess("chase")
 
 
+agent = MyAgent()
+print(agent.rand_green_not_absent([None, None,None, None, 's'], []))
 
+
+
+# </myQwordleEnv.py>
